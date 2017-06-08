0530667ce0dab1e684895036c1d76140754c8391
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-06-08 21:00:35 +0200

    javascript trie par catégorie au click

--- js/script.js
+++ js/script.js
@@ -3,8 +3,29 @@ $(document).ready(function(){
   $('nav a').click(function(){
     $('nav li.current').removeClass('current');
     $(this).parent().addClass('current');
-  });
 
   //set heading text
   $('h1#heading').text($(this).text());
-})
+
+  //Get and filter link text
+  var category = $(this).text().toLowerCase().replace(' ', '-');
+
+  //remove hidden class if "all projects" is selected
+  if(category == 'all-projects'){
+    $('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+  } else {
+    $('ul#gallery li').each(function(){
+      if(!$(this).hasClass(category)){
+        $(this).hide().addClass('hidden');
+      } else {
+        $(this).fadeIn('slow').removeClass('hidden');
+      }
+    })
+  }
+  //Stop link behaviour
+  return false;
+
+
+
+  });
+});
