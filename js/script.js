65381bc28368a0f9c5675591b8d3aa7f57aa0ee2
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-06-09 11:43:59 +0200

    javascript overlay effect ok

--- js/script.js
+++ js/script.js
@@ -24,8 +24,37 @@ $(document).ready(function(){
   }
   //Stop link behaviour
   return false;
+  });
 
+  //mouseenter overlay
+  $('ul#gallery li').mouseenter(function(){
+    //Get data-attribute values
+    var title = $(this).children().data('title');
+    var desc = $(this).children().data('desc');
+    //Validation
+    if(desc == null){
+      desc='Click to enlarge';
+    }
+    if(title == null){
+      title=' ';
+    }
+    //Create overlay div
+    $(this).append('<div class="overlay"></div>')
+    //Get the overlay div
+    var overlay = $(this).children('.overlay');
+    //Add html to overlay
+    overlay.html('<h3>' + title + '</h3>' + '<p>' + desc + '</p>');
+    //Fadein the overlay
+    overlay.fadeIn(800);
+  })
 
-
+  //mouseleave overlay
+  $('ul#gallery li').mouseleave(function(){
+    //Create overlay div
+    $(this).append('<div class="overlay"></div>')
+    //Get the overlay div
+    var overlay = $(this).children('.overlay');
+    //fadeOut overlay
+    overlay.fadeOut(500);
   });
 });
