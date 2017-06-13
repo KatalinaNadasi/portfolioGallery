67baf71fc2d105946cb2292036d0f1ec77aba418
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-06-13 11:28:09 +0200

    responsive ok

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-const randomize = (arr) => {
-    for(var i = 0; i < arr.length; i++){
-      const random = Math.floor(Math.random() * arr.length);
-
-      [arr[i], arr[random]] = [arr[random], arr[i]];
-    }
-    return arr;
-}
-console.log(randomize([1, 2, 3, 4, 5]));
-
-// function randomize(arr){
-//
-//
-//   for(var i = 0; i < arr.length; i++){
-//     random = Math.floor(Math.random() * arr.length);
-//     temps = arr[i];
-//     arr[i] = arr[random];
-//     arr[random] = temps;
-//   }
-//   return arr;
-// }
-//
-// console.log(randomize([1, 2, 3, 4, 5]));
-
-
-
 $(document).ready(function(){
 
   //Current class assignement
