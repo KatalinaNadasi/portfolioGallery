6d2a2993abdc8f59d82ddda89e2e8bd42c3bed72
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-06-13 11:08:29 +0200

    rajout lightbox

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
+const randomize = (arr) => {
+    for(var i = 0; i < arr.length; i++){
+      const random = Math.floor(Math.random() * arr.length);
+
+      [arr[i], arr[random]] = [arr[random], arr[i]];
+    }
+    return arr;
+}
+console.log(randomize([1, 2, 3, 4, 5]));
+
+// function randomize(arr){
+//
+//
+//   for(var i = 0; i < arr.length; i++){
+//     random = Math.floor(Math.random() * arr.length);
+//     temps = arr[i];
+//     arr[i] = arr[random];
+//     arr[random] = temps;
+//   }
+//   return arr;
+// }
+//
+// console.log(randomize([1, 2, 3, 4, 5]));
+
+
+
 $(document).ready(function(){
+
   //Current class assignement
   $('nav a').click(function(){
     $('nav li.current').removeClass('current');
